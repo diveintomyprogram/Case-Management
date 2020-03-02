67811de7a5a3a2b9eb38ac4efbd161dec07851d4
Daniel Roberts <ddanielr@gmail.com>
2020-03-01 22:19:01 -0500

    Fixing Formatting
    Fix the formatting of Main class

--- Main.java
+++ Main.java
@@ -3,20 +3,19 @@ import java.sql.DriverManager;
 
 public class Main {
    public static void main(String args[]) {
-	String dbHost = "null";
-   	try {
-      		String tmp = System.getenv("DATABASE_HOST");
-		dbHost = tmp;
+	var dbHost = "";
+  	try {
+         dbHost = System.getenv("DATABASE_HOST");
 	}
 	catch(NullPointerException e) {
 		System.out.println("Cannot find value 'DATABASE_HOST'");
-       }
-      Connection c = null;
-      try {
-         Class.forName("org.postgresql.Driver");
-         c = DriverManager.getConnection("jdbc:postgresql://" + dbHost + ":5432/case",
-	 				 "CMSC495",
-					 "SuperSecret");
+   }
+   Connection c = null;
+   try {
+      Class.forName("org.postgresql.Driver");
+      c = DriverManager.getConnection("jdbc:postgresql://" + dbHost + ":5432/case",
+	 	                                "CMSC495",
+					                       "SuperSecret");
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName()+": "+e.getMessage());
